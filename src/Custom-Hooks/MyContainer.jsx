687abe9ba19fddc2508f1eeffb61c875db9553e0
thesmahmud77 <mahmudsazzad77@gmail.com>
2026-05-11 23:34:23 +0600

    Bug Fixing

--- src/Custom-Hooks/MyContainer.jsx
+++ src/Custom-Hooks/MyContainer.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
-import Footer from "../Components/Footer";
 
 const MyContainer = ({ children }) => {
   return (
     <div className="max-w-7xl min-h-screen mx-auto text-cent">
       <Navbar></Navbar>
       {children}
-      <Footer></Footer>
     </div>
   );
 };
